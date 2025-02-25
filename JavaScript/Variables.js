bf1e0e8a746db207762458f40b4efe2953bbf57d
devkunal50 <wankhadekunal77@gmail.com>
2025-02-25 08:33:48 +0000

    container config done

---
+++ JavaScript/Variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "wankhadekunal77@gmail.com"
+var accountPassword = "15423"
+accountCity = "Amravati"
+
+//let accountState ;
+
+accountId = 2
+accountEmail = "Wankhadekunal777@gmail.com"
+accountPassword = "kun@l.googlepassword"
+accountCity = "Mumbai"
+
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+ 
