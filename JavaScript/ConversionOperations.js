64a4521c3f505f153d6ff974e2045dc090e08e17
devkunal50 <wankhadekunal77@gmail.com>
2025-02-26 10:12:26 +0000

    container config done

---
+++ JavaScript/ConversionOperations.js
@@ -0,0 +1,21 @@
+// let value =  3
+
+// let negVal = value
+
+// console.log();
+
+// let str1 = "hello"
+// let str2 = " kunal"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(+true);
+// console.log(+"");
+
+//  let gameCounter =  24
+//  gameCounter++
+
+//  console.log(gameCounter);
+ 
